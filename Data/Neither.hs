39ddc38dfcd467bfcce1e92540c94d84757a5f66
Anselm Schüler <mail@anselmschueler.com>
2022-02-16 20:10:54 +0100

    Move Haddock comments to “-- |” format & rebuild haddock docs

--- Data/Neither.hs
+++ Data/Neither.hs
@@ -28,44 +28,44 @@ import Data.Bits
 
 data Neither a b = Neither deriving (Read, Show, Eq, Ord, Enum, Bounded, Data)
 
+-- | Produces a Neither value whose types match the arguments, discarding the arguments
 neither :: a -> b -> Neither a b
 neither _ _ = Neither
--- ^ Produces a Neither value whose types match the arguments, discarding the arguments
 
+-- | An empty container
 instance Functor (Neither a) where
--- ^ An empty container
     fmap _ _ = Neither
     _ <$ _ = Neither
 
+-- | An empty container
 instance Applicative (Neither a) where
--- ^ An empty container
     pure _ = Neither
     _ <*> _ = Neither
     _ *> _ = Neither
     _ <* _ = Neither
 
+-- | An empty container
 instance Monad (Neither a) where
--- ^ An empty container
     _ >>= _ = Neither
 
+-- | An empty container
 instance MonadFail (Neither a) where
--- ^ An empty container
     fail _ = Neither
 
+-- | An empty container
 instance MonadIO (Neither a) where
--- ^ An empty container
     liftIO _ = Neither
 
+-- | The trivial single element semigroup
 instance Semigroup (Neither a b) where
--- ^ The trivial single element semigroup
     _ <> _ = Neither
 
+-- | The trivial single element monoid
 instance Monoid (Neither a b) where
--- ^ The trivial single element monoid
     mempty = Neither
 
+-- | A trivial single element group with no-ops, where every integer is Neither
 instance Num (Neither a b) where
--- ^ A trivial single element group with no-ops, where every integer is Neither
     _ + _ = Neither
     _ - _ = Neither
     _ * _ = Neither
@@ -74,12 +74,12 @@ instance Num (Neither a b) where
     signum _ = Neither
     fromInteger _ = Neither
 
+-- | A number type that only contains zero
 instance Real (Neither a b) where
--- ^ A number type that only contains zero
     toRational _ = 0
 
+-- | A number type that only contains zero, or a trivial single element euclidean domain
 instance Integral (Neither a b) where
--- ^ A number type that only contains zero, or a trivial single element euclidean domain
     quot _ _ = Neither
     rem _ _ = Neither
     div _ _ = Neither
@@ -88,14 +88,14 @@ instance Integral (Neither a b) where
     divMod _ _ = (Neither, Neither)
     toInteger _ = 0
 
+-- | A trivial single element field where every ratio is Neither
 instance Fractional (Neither a b) where
--- ^ A trivial single element field where every ratio is Neither
     _ / _ = Neither
     recip _ = Neither
     fromRational _ = Neither
 
+-- | A trivial single element exponential field, where every number is Neither
 instance Floating (Neither a b) where
--- ^ A trivial single element exponential field, where every number is Neither
     pi = Neither
     exp _ = Neither
     log _ = Neither
@@ -115,16 +115,16 @@ instance Floating (Neither a b) where
     acosh _ = Neither
     atanh _ = Neither
 
+-- | A number type that only contains zero
 instance RealFrac (Neither a b) where
--- A number type that only contains zero
     properFraction _ = (0, Neither)
     truncate _ = 0
     round _ = 0
     ceiling _ = 0
     floor _ = 0
 
+-- | A bitstring with zero length
 instance Bits (Neither a b) where
--- ^ A bitstring with zero length
     _ .&. _ = Neither
     _ .|. _ = Neither
     xor _ _ = Neither
@@ -146,47 +146,47 @@ instance Bits (Neither a b) where
     rotateR _ _ = Neither
     popCount _ = 0
 
+-- | A bitstring with zero length
 instance FiniteBits (Neither a b) where
--- ^ A bitstring with zero length
     finiteBitSize _ = 0
     countLeadingZeros _ = 0
     countTrailingZeros _ = 0
 
+-- | An empty container with no-ops
 instance Alternative (Neither a) where
--- ^ An empty container with no-ops
     empty = Neither
     _ <|> _ = Neither
     some _ = Neither
     many _ = Neither
 
+-- | An empty container with no-ops
 instance MonadPlus (Neither a) where
--- ^ An empty container with no-ops
     mzero = Neither
     mplus _ _ = Neither
 
+-- | An empty container or an uncallable or no-op function representation
 instance Contravariant (Neither a) where
--- ^ An empty container or an uncallable or no-op function representation
     contramap _ _ = Neither
     _ >$ _ = Neither
 
+-- | An empty container
 instance Bifunctor Neither where
--- ^ An empty container
     bimap _ _ _ = Neither
     first _ _ = Neither
     second _ _ = Neither
 
+-- | An empty container
 instance MonadZip (Neither a) where
--- ^ An empty container
     mzip _ _ = Neither
     mzipWith _ _ _ = Neither
     munzip _ = (Neither, Neither)
 
+-- | All functions from Neither to Neither must have Neither as a fixed point (if you ignore types)
 instance MonadFix (Neither a) where
--- ^ All functions from Neither to Neither must have Neither as a fixed point (if you ignore types)
     mfix _ = Neither
 
+-- | An empty container which folds to the starting value or the identity
 instance Foldable (Neither a) where
--- ^ An empty container which folds to the starting value or the identity
     foldMap _ _ = mempty
     foldr _ x _ = x
     foldl _ x _ = x
@@ -194,84 +194,84 @@ instance Foldable (Neither a) where
     sum _ = 0
     product _ = 1
 
+-- | An empty container, that, when in an applicative, must always be a minimally wrapped constant value
 instance Traversable (Neither a) where
--- ^ An empty container, that, when in an applicative, must always be a minimally wrapped constant value
     traverse _ _ = pure Neither
     sequenceA _ = pure Neither
     mapM _ _ = pure Neither
     sequence _ = pure Neither
 
+-- | An empty container which folds to the starting value or the identity
 instance Bifoldable Neither where
--- ^ An empty container which folds to the starting value or the identity
     bifold _ = mempty
     bifoldMap _ _ _ = mempty
     bifoldr _ _ x _ = x
     bifoldl _ _ x _ = x
 
+-- | An empty container, that, when in an applicative, must always be a minimally wrapped constant value
 instance Bitraversable Neither where
--- ^ An empty container, that, when in an applicative, must always be a minimally wrapped constant value
     bitraverse _ _ _ = pure Neither
 
+-- | The constant functor from Set to 1
 instance Category Neither where
--- ^ The constant functor from Set to 1
     id = Neither
     _ . _ = Neither
 
+-- | A dummy function representation
 instance Arrow Neither where
--- ^ A dummy function representation
     arr _ = Neither
     first _ = Neither
     second _ = Neither
     _ *** _ = Neither
     _ &&& _ = Neither
 
+-- | A dummy function representation, so the zero arrow is merely unique by its type parameters
 instance ArrowZero Neither where
--- ^ A dummy function representation, so the zero arrow is merely unique by its type parameters
     zeroArrow = Neither
 
+-- |     The trivial single element monoid on a dummy function representation
 instance ArrowPlus Neither where
--- ^ The trivial single element monoid on a dummy function representation
     _ <+> _ = Neither
 
+-- | A dummy function representation, where all choices are the same
 instance ArrowChoice Neither where
--- ^ A dummy function representation, where all choices are the same
     left _ = Neither
     right _ = Neither
     _ +++ _ = Neither
     _ ||| _ = Neither
 
+-- | A dummy function representation, where application returns another dummy
 instance ArrowApply Neither where
--- ^ A dummy function representation, where application returns another dummy
     app = Neither
 
+-- | A dummy function representation, where recursion creation returns another dummy
 instance ArrowLoop Neither where
--- ^ A dummy function representation, where recursion creation returns another dummy
     loop _ = Neither
 
+-- | Every string is Neither
 instance IsString (Neither a b) where
--- ^ Every string is Neither
     fromString _ = Neither
 
+-- | There is only one Neither value
 instance Ix (Neither a b) where
--- ^ There is only one Neither value
     range _ = [Neither]
     index _ _ = 0
     inRange _ _ = True
     rangeSize _ = 1
 
+-- | Empty container that is equal to itself (with no values to compare)
 instance Eq1 (Neither a) where
--- ^ Empty container that is equal to itself (with no values to compare)
     liftEq _ _ _ = True
 
+-- | Empty container that is equal to itself (with no values to compare)
 instance Ord1 (Neither a) where
--- ^ Empty container that is equal to itself (with no values to compare)
     liftCompare _ _ _ = EQ
 
+-- | Empty container that is equal to itself (with no values to compare)
 instance Eq2 Neither where
--- ^ Empty container that is equal to itself (with no values to compare)
     liftEq2 _ _ _ _ = True
 
+-- | Empty container that is equal to itself (with no values to compare)
 instance Ord2 Neither where
--- ^ Empty container that is equal to itself (with no values to compare)
     liftCompare2 _ _ _ _ = EQ
 
